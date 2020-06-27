a5458a934a55e8d933899006a6aef5b130ebe3e0
hiroki4455 <tue0_0125@yahoo.co.jp>
2020-06-27 15:50:10 +0900

    イイネボタン追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,17 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
-    @like.save
+    if @like.save
+      redirect_back fallback_location: root_path, success: 'いいねしました'
+    else
+      render :edit
+      flash.now['danger'] = '編集に失敗しました'
+    end
   end
 
   def destroy
     @like = Like.find(params[:post_id])
     @like.destroy
+    redirect_back fallback_location: root_path, danger: 'いいねを削除しました'
   end
 end
