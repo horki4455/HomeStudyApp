11505663da91b0fe6b8b79b5d000640f78d68ae4
hiroki4455 <tue0_0125@yahoo.co.jp>
2020-07-03 21:37:54 +0900

    crud機能作成　comment

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,7 +1,8 @@
 class Admin::CommentsController < Admin::BaseController
 
   def index
-    @comments = Comment.all.includes(:board).order(created_at: :desc)
+    @q = Comment.ransack(params[:q])
+    @comments = @q.result(distinct: true).includes(:board).order(created_at: :desc)
   end
 
   def create
@@ -12,8 +13,32 @@ class Admin::CommentsController < Admin::BaseController
       redirect_to admin_board_path(@comment.board), danger: '作成失敗'
     end
   end
+
+  def edit
+    @comment = current_user.comments.find(params[:id])
+  end
+
+  def update
+    @comment = current_user.comments.find(params[:id])
+    if @comment.update(comment_update_params)
+      redirect_to(admin_comments_path, success: '掲示板を更新しました。')
+    else
+      flash.now[:danger] = '掲示板の更新に失敗しました。'
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to(admin_comments_path, danger:  '投稿を削除しました')
+  end
   
   def comment_params
     params.require(:comment).permit(:body).merge(board_id: params[:board_id])
+  end
+
+  def comment_update_params
+    params.require(:comment).permit(:body)
   end
 end
